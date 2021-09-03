563489fb7ceab82f6c54a49963fef2c06d49e719
syhanjin <2819469337@qq.com>
2021-09-03 12:10:02 +0800

    test1

--- bot.py
+++ bot.py
@@ -2,16 +2,17 @@
 # -*- coding: utf-8 -*-
 import nonebot
 from nonebot.adapters.cqhttp import Bot as CQHTTPBot
-#初始化nb
+# 初始化nb
 nonebot.init()
-app = nonebot.get_asgi()
-#连接驱动
+
+# 连接驱动
 driver = nonebot.get_driver()
 driver.register_adapter("cqhttp", CQHTTPBot)
-#加载插件(除此处其他配置不建议更改)
-nonebot.load_builtin_plugins()
 
-#启动bot
+# 加载插件(除此处其他配置不建议更改)
+nonebot.load_builtin_plugins()
+nonebot.load_plugins('src/plugins')
+
+app = nonebot.get_asgi()
 if __name__ == "__main__":
-    nonebot.logger.warning("Always use `nb run` to start the bot instead of manually running!")
-    nonebot.run(app="__mp_main__:app")
+    nonebot.run()
---
+++ src/plugins/checkin/__init__.py
@@ -0,0 +1,63 @@
+import datetime
+import os
+import random
+
+import nonebot
+from nonebot import on_command
+from nonebot.adapters import Bot, MessageEvent
+from nonebot.typing import T_State
+from PIL import Image, ImageDraw, ImageFont
+from utils.message_builder import image
+
+checkin = on_command("签到", priority=5)
+
+# -- 数据库 --
+import pymongo
+
+client = pymongo.MongoClient('127.0.0.1', 27017)
+db = client['qbot']
+userdb = client['user']
+# -- ------ --
+
+NAME = list(nonebot.config.nickname)[0]
+root_path = os.path.join('.', 'res', 'sign')
+card_ratio = 16 / 9
+
+async def rand(st, ed): return random.random()*(ed-st) + st
+
+
+async def putText(draw, x, y, text: 'str | tuple', font='微软雅黑', fontsize=16, fill=(0, 0, 0), border=0, borderFill=(0, 0, 0)):
+    font = ImageFont.truetype(font, size=fontsize)
+    if type(text) == type(()):
+        tmp = text
+        text = ''
+        for i in tmp:
+            text += str(i) + ' '
+    text = str(text)
+    if border > 0:
+        # 文字阴影
+        # thin border
+        draw.text((x-border, y), text, font=font, fill=borderFill)
+        draw.text((x+border, y), text, font=font, fill=borderFill)
+        draw.text((x, y-border), text, font=font, fill=borderFill)
+        draw.text((x, y+border), text, font=font, fill=borderFill)
+        # thicker border
+        draw.text((x-border, y-border), text, font=font, fill=borderFill)
+        draw.text((x+border, y-border), text, font=font, fill=borderFill)
+        draw.text((x-border, y+border), text, font=font, fill=borderFill)
+        draw.text((x+border, y+border), text, font=font, fill=borderFill)
+    draw.text((x, y), text, font=font, fill=fill)
+    return draw.textsize(text, font=font, spacing=0)
+
+
+async def create_data(msg):
+    data = {'qq': msg['user_id'], 'group': msg['group_id']}
+    data['favorLevel'] = 0      # 好感等级
+    data['continuity'] = 0      # 连续签到
+    data['last'] = None         # 上次签到时间
+    return data
+
+@checkin.handle()
+async def _(bot: Bot, event: MessageEvent, state: T_State):
+    print(event.get_session_id())
+    # data = db.user.find_one({'group_id': event.grou})
