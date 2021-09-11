bbd63f8912c780d73c2abb59ac09a9cf3a55ebb7
syhanjin <2819469337@qq.com>
2021-09-11 08:51:13 +0800

    test

--- src/plugins/notices/__init__.py
+++ src/plugins/notices/__init__.py
@@ -1,12 +1,11 @@
 
 import datetime
-from threading import Event
 from handles import User
 import json
 import nonebot
 from nonebot import on_notice
-from nonebot.adapters.cqhttp.utils import escape
-from nonebot.adapters.cqhttp import Bot, GroupIncreaseNoticeEvent
+from nonebot.permission import SUPERUSER
+from nonebot.adapters.cqhttp import Bot, GroupIncreaseNoticeEvent, GroupBanNoticeEvent
 from nonebot.adapters.cqhttp.message import MessageSegment as ms
 from nonebot.adapters.cqhttp.permission import GROUP
 import pymongo
@@ -19,11 +18,22 @@ increase = on_notice(priority=2, block=False)
 
 
 @increase.handle()
-async def _(bot: Bot, event: GroupIncreaseNoticeEvent):
+async def _increase(bot: Bot, event: GroupIncreaseNoticeEvent):
     data = db.increase.find_one({'group_id': event.group_id})
     if data == None:
         msg = '欢迎新成员，请先看群公告~~~'
     else:
         msg = data['msg']
-    await bot.send(event,f"{msg}", at_sender=True)
+    await bot.send(event, f"{msg}", at_sender=True)
     return
+
+
+disban = on_notice(property=1, block=False, permission=GROUP | SUPERUSER)
+
+
+@disban.handle()
+async def _disban(bot: Bot, event: GroupBanNoticeEvent):
+
+    await bot.send(event, ms.text('禁言事件测试：Event => ')+ms.text(str(event.json)))
+    # if event.user_id != 'all'
+    # await bot.call_api('set_group_ban')
