0030001442dbd0f1175d3edcd379cb94b34d2d27
syhanjin <2819469337@qq.com>
2021-09-18 22:22:42 +0800

    bug

--- src/plugins/special.py
+++ src/plugins/special.py
@@ -4,7 +4,7 @@ from nonebot.exception import IgnoredException
 from nonebot.adapters.cqhttp.bot import Bot
 from nonebot.adapters.cqhttp.event import Event, GroupMessageEvent
 from nonebot.matcher import Matcher
-from nonebot.message import run_postprocessor
+from nonebot.message import run_preprocessor
 from nonebot.typing import T_State
 
 @run_preprocessor
