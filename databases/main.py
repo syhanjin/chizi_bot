dcf7efe7bafedaac70a3de82732b6ead939952a1
syhanjin <2819469337@qq.com>
2021-09-26 20:29:55 +0800

    icon

--- databases/main.py
+++ databases/main.py
@@ -10,11 +10,11 @@ db.increase.insert_many([
     {
         'group_id': 457263503,
         'type': 'card',
+        'icon': 'https://sakuyark.com/static/images/yyicon.jpg',
         'tips': [('请仔细查看公告内的群规',), ('格式 [C/K+]班级[+学号]-名字', '名片格式')],
         'text': '''欢迎新同学''',
         'opened': True
-    },
-    {
+    },{
         'group_id': 1003132999,
         'type': 'card',
         'tips': [('请仔细查看公告内的群规',), ('格式 [C/K+]班级[+学号]-名字', '名片格式')],
