399a09b51556406d07790a7b07777de97c65809a
syhanjin <2819469337@qq.com>
2021-09-26 20:09:28 +0800

    kick

--- src/plugins/group_commands/__init__.py
+++ src/plugins/group_commands/__init__.py
@@ -3,6 +3,7 @@ import re
 import pymongo
 
 from nonebot import on_command
+from nonebot.adapters.cqhttp.event import GroupMessageEvent
 from nonebot.adapters.cqhttp import Bot, MessageEvent
 from nonebot.adapters.cqhttp.message import MessageSegment as ms
 from nonebot.adapters.cqhttp.permission import GROUP_ADMIN, GROUP_OWNER
@@ -10,16 +11,18 @@ from nonebot.permission import SUPERUSER
 from nonebot.typing import T_State
 
 set_ban = on_command(
-    "禁言", priority=3, permission=GROUP_ADMIN | GROUP_OWNER | SUPERUSER
+    "禁言", priority=3, permission=GROUP_ADMIN | GROUP_OWNER | SUPERUSER, block=True
 )
 cancel_ban = on_command(
     "解禁", aliases={'解除禁言'}, priority=3, permission=GROUP_ADMIN | GROUP_OWNER | SUPERUSER, block=True
 )
-
+kick = on_command(
+    "踢出", priority=3, permission=GROUP_ADMIN | GROUP_OWNER | SUPERUSER, block=True
+)
 
 
 @set_ban.handle()
-async def handle_first_receive(bot: Bot, event: MessageEvent, state: T_State):
+async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
     args = str(event.get_message()).strip()
     r = re.compile("\[CQ:at,qq=(\d+)\] *(\d+)?")
     if not r.match(args):
@@ -32,20 +35,20 @@ async def handle_first_receive(bot: Bot, event: MessageEvent, state: T_State):
     if qq == 'all':
         await bot.call_api(
             'set_group_whole_ban',
-            group_id=json.loads(event.json())['group_id'],
+            group_id=event.group_id,
             enable=(not(duration == 0))
         )
         return
     await bot.call_api(
         'set_group_ban',
         user_id=int(qq),
-        group_id=json.loads(event.json())['group_id'],
+        group_id=event.group_id,
         duration=duration
     )
 
 
 @cancel_ban.handle()
-async def handle_first_receive(bot: Bot, event: MessageEvent, state: T_State):
+async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
     args = str(event.get_message()).strip()
     r = re.compile("\[CQ:at,qq=(\d+)\]")
     if not r.match(args):
@@ -54,15 +57,31 @@ async def handle_first_receive(bot: Bot, event: MessageEvent, state: T_State):
     if qq == 'all':
         await bot.call_api(
             'set_group_whole_ban',
-            group_id=json.loads(event.json())['group_id'],
+            group_id=event.group_id,
             enable=False
         )
         return
     await bot.call_api(
         'set_group_ban',
         user_id=int(qq),
-        group_id=json.loads(event.json())['group_id'],
+        group_id=event.group_id,
         duration=0
     )
 
 
+@kick.handle()
+async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
+    args = str(event.get_message()).strip()
+    r = re.compile("\[CQ:at,qq=(\d+)\]")
+    if not r.match(args):
+        await bot.send(event, ms.text('格式错误, 标准格式：解禁 <@某人>'))
+    qq = r.match(args).group(1)
+    if qq == 'all':
+        await bot.send(event, '不支持全员踢出！')
+        return
+    await bot.call_api(
+        'set_group_kick',
+        user_id=int(qq),
+        group_id=event.group_id
+    )
+    
