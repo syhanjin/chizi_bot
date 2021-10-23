74938d464e25ccddee2faffe30fdd2b713a511af
syhanjin <2819469337@qq.com>
2021-10-23 21:44:47 +0800

    1002

---
+++ src/plugins/website/__init__.py
@@ -0,0 +1,25 @@
+import nonebot
+import asyncio
+import subprocess
+from nonebot.permission import SUPERUSER
+from nonebot.adapters.cqhttp.permission import PRIVATE
+from nonebot.plugin import on_command
+from nonebot.adapters.cqhttp.bot import Bot
+from nonebot.adapters.cqhttp.event import PrivateMessageEvent
+
+restart = on_command(
+    'website restart', aliases={'网站重启', '重启网站'},
+    priority=2, block=True, permission=SUPERUSER | PRIVATE
+)
+
+
+@restart.handle()
+async def _(bot: Bot, event: PrivateMessageEvent):
+    subp = subprocess.Popen(
+        '/root/restart_website.sh', stdout=subprocess.PIPE, encoding="utf-8"
+    )
+    while True:
+        if subp.poll() is not None and subp.poll() == 0:
+            await bot.send(event, f'''重启成功，输出：\n{subp.communicate()[1]}''')
+            return
+        await asyncio.sleep(1)
