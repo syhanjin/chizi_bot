750720271e8b602d12664e9c2c5293780dab40be
syhanjin <2819469337@qq.com>
2021-10-24 11:11:47 +0800

    1032

--- handles/__init__.py
+++ handles/__init__.py
@@ -1,5 +1,6 @@
 import nonebot
 
 
-
 NAME = list(nonebot.get_driver().config.nickname)[0]
+
+ONWER = int(list(nonebot.get_bot().config.superusers)[0])
--- src/plugins/console/__init__.py
+++ src/plugins/console/__init__.py
@@ -10,6 +10,7 @@ from nonebot.adapters.cqhttp.permission import PRIVATE
 from nonebot.permission import SUPERUSER
 from nonebot.plugin import load_plugins, on_command, require
 from nonebot.typing import T_State
+from handles import ONWER
 
 from handles.message_builder import text
 
@@ -28,23 +29,33 @@ update = on_command(
 async def _(bot: Bot, event: PrivateMessageEvent):
     subp = subprocess.Popen('./restart.sh', shell=True)
 
+
 @scheduler.scheduled_job("cron", hour=0, id="update")
 async def bot_update():
     bot = get_bot()
-    await bot.send(text('检查更新...'))
+    await bot.send_private_msg(
+        user_id=ONWER,
+        message=text('检查更新...')
+    )
     pull = subprocess.Popen(
         'git pull origin master',
         shell=True, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE
     )
     while pull.poll() is None:
         await asyncio.sleep(1)
-    await bot.send(
-        '$ git pull origin master\n'
-        + str(pull.communicate()[1], encoding='utf-8')
-        + str(pull.communicate()[0], encoding='utf-8')
+    await bot.send_private_msg(
+        user_id=ONWER,
+        message=text(
+            '$ git pull origin master\n'
+            + str(pull.communicate()[1], encoding='utf-8')
+            + str(pull.communicate()[0], encoding='utf-8')
+        )
     )
     await asyncio.sleep(1)
-    await bot.send(text('重启bot...'))
+    await bot.send_private_msg(
+        user_id=ONWER,
+        message=text('重启bot...')
+    )
     subprocess.Popen('./restart.sh', shell=True)
 
 
