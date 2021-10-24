da740a7d4cb8352c0fc7238dda3759ee250f872c
syhanjin <2819469337@qq.com>
2021-10-24 11:06:03 +0800

    1030

--- src/plugins/console/__init__.py
+++ src/plugins/console/__init__.py
@@ -2,16 +2,16 @@ import asyncio
 import os
 import subprocess
 import nonebot
-from nonebot import permission
+from nonebot import get_bot, permission
 from nonebot import matcher
 from nonebot.adapters.cqhttp.bot import Bot
 from nonebot.adapters.cqhttp.event import PrivateMessageEvent
 from nonebot.adapters.cqhttp.permission import PRIVATE
 from nonebot.permission import SUPERUSER
-from nonebot.plugin import load_plugins, on_command
+from nonebot.plugin import load_plugins, on_command, require
 from nonebot.typing import T_State
 
-
+scheduler = require("nonebot_plugin_apscheduler").scheduler
 restart = on_command(
     '重启', aliases={'bot重启'},
     priority=2, block=True, permission=SUPERUSER | PRIVATE
@@ -26,9 +26,10 @@ update = on_command(
 async def _(bot: Bot, event: PrivateMessageEvent):
     subp = subprocess.Popen('./restart.sh', shell=True)
 
-
-@update.handle()
-async def _(bot: Bot, event: PrivateMessageEvent, state: T_State):
+@scheduler.scheduled_job("cron", hour=0, id="update")
+async def bot_update():
+    bot = get_bot()
+    await bot.send('检查更新...')
     pull = subprocess.Popen(
         'git pull origin master',
         shell=True, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE
@@ -36,11 +37,15 @@ async def _(bot: Bot, event: PrivateMessageEvent, state: T_State):
     while pull.poll() is None:
         await asyncio.sleep(1)
     await bot.send(
-        event,
         '$ git pull origin master\n'
-        + str(pull.communicate()[0], encoding='utf-8')
         + str(pull.communicate()[1], encoding='utf-8')
+        + str(pull.communicate()[0], encoding='utf-8')
     )
     await asyncio.sleep(1)
-    await bot.send(event, '重启中...')
-    subp = subprocess.Popen('./restart.sh', shell=True)
+    await bot.send('重启中...')
+    subprocess.Popen('./restart.sh', shell=True)
+
+
+@update.handle()
+async def _(bot: Bot, event: PrivateMessageEvent, state: T_State):
+    await bot_update()
