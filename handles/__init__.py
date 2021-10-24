f58cc024592083fd0a228ea9c1f1d9019041e3b6
syhanjin <2819469337@qq.com>
2021-10-24 11:12:27 +0800

    1033

--- handles/__init__.py
+++ handles/__init__.py
@@ -3,4 +3,4 @@ import nonebot
 
 NAME = list(nonebot.get_driver().config.nickname)[0]
 
-ONWER = int(list(nonebot.get_bot().config.superusers)[0])
+ONWER = int(list(nonebot.get_driver().config.superusers)[0])
