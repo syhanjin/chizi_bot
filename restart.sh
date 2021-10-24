e6e7200a34c857c8310db593a6410c13f122209f
syhanjin <2819469337@qq.com>
2021-10-24 09:45:51 +0800

    1015

--- restart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source activate qbot
-cd ~/pyqbot/chizi_bot
-
-kill -s 9 `ps -aux | grep 'python bot.py' | awk '{print $2}'`
-
-screen -x qbot
-python bot.py
--- start.sh
+++
@@ -1,7 +0,0 @@
-source activate qbot
-cd ~/QQBOT
-kill -s 9 `ps -aux | grep 'go-cqhttp' | awk '{print $2}'`
-nohup ./go-cqhttp faststart > log 2>&1 &
-
-cd ~/pyqbot/chizi_bot
-python bot.py
